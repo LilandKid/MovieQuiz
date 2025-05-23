6da135ad4313746eecef70bcea2079bb10c86539
овощ <prostoroker@mail.ru>
2025-05-23 22:14:06 +0300

    Страшно

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,3 +1,16 @@
 import UIKit
 
-extension UIColor { }
+extension UIColor {
+         static var ypGreen: UIColor { UIColor(named: "Green YP") ?? UIColor.green }
+         static var ypRed: UIColor { UIColor(named: "Red YP") ?? UIColor.red }
+         static var ypBlack: UIColor { UIColor(named: "Black YP") ?? UIColor.black}
+         static var ypBackground: UIColor { UIColor(named: "Back YP") ?? UIColor.darkGray }
+         static var ypGray: UIColor { UIColor(named: "Grey YP") ?? UIColor.gray }
+         static var ypWhite: UIColor { UIColor(named: "White YP") ?? UIColor.white}
+    }
+    
+    
+    
+    
+    
+
